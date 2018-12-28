448c4196b4176490c97a95b698f15118d5c8a9d5
manzar <manzarrizvi92@gmail.com>
2018-12-28 22:11:56 +0500

    Refactor and clean out layout

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,9 @@
 class RecipesController < ApplicationController
+	before_action :set_recipe, only: [:show, :edit, :update]
 	def index
 		@recipes = Recipe.all
 	end
 	def show
-		@recipe = Recipe.find(params[:id])
 	end
 	def new
 		@recipe = Recipe.new
@@ -20,10 +20,8 @@ class RecipesController < ApplicationController
 	end
 
 	def edit
-		@recipe = Recipe.find(params[:id])
 	end
 	def update
-		@recipe = Recipe.find(params[:id])
 		if @recipe.update(recipe_params)
 			flash[:success] = "Recipe was updated successfully!"
       		redirect_to recipe_path(@recipe)
@@ -37,6 +35,10 @@ class RecipesController < ApplicationController
 		redirect_to recipes_path
 	end
 	private 
+
+	def set_recipe
+	  @recipe = Recipe.find(params[:id])
+	end
 
 	def recipe_params
 		params.require(:recipe).permit(:name, :description)
--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -24,14 +24,15 @@ class RecipesTest < ActionDispatch::IntegrationTest
 
     end
 
-    test "should get recipes show" do
-      get recipe_path(@recipe)
-      assert_template 'recipes/show'
-      assert_match @recipe.name, response.body
-      assert_match @recipe.description, response.body
-      assert_match @chef.chefname, response.body
-      assert_select 'a[href=?]', edit_recipe_path(@recipe), text: "Edit this recipe"
-      assert_select 'a[href=?]', recipe_path(@recipe), text: "Delete this recipe"
+   test "should get recipes show" do
+    get recipe_path(@recipe)
+    assert_template 'recipes/show'
+    assert_match @recipe.name, response.body
+    assert_match @recipe.description, response.body
+    assert_match @chef.chefname, response.body
+    assert_select 'a[href=?]', edit_recipe_path(@recipe), text: "Edit this recipe"
+    assert_select 'a[href=?]', recipe_path(@recipe), text: "Delete this recipe"
+    assert_select 'a[href=?]', recipes_path, text: "Return to recipes listing"
 end
 
       test "create new valid recipe" do
