b5f8fdcdf439af76de8c17d7f69bfb1019939172
manzar <manzarrizvi92@gmail.com>
2018-12-30 20:19:00 +0500

    Add editaction with tests

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -8,7 +8,5 @@ class Chef < ApplicationRecord
 
 	has_many :recipes
 	has_secure_password
-	validates :password, presence: true, length: {minimum: 5}
-
-
+	validates :password, presence: true, length: { minimum: 5 }, allow_nil: true
 end
